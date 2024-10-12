8df391081c375aaab3f7eb477649f0e87b5677c9
Gustavo França <gustavofr0411@gmail.com>
2024-10-12 19:39:16 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("layout.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene screen = new Scene(root);
+
+        arg0.setTitle("This is a test");
+        arg0.setScene(screen);
+        arg0.show();
+        // test
+    }
+}
