3b27157d560d3aca52032e1ee5f29ba0cc96495f
Gustavo França <gustavofr0411@gmail.com>
2024-10-12 21:39:43 -0300

    Testing a table using GridPane

--- src/Controller.java
+++ src/Controller.java
@@ -2,6 +2,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class Controller {
 
@@ -9,11 +10,28 @@ public class Controller {
     private Button button;
 
     @FXML
-    private Label label;
+    private GridPane grid;
 
     @FXML
-    void onPressed(ActionEvent event) {
-        label.setText("The text has been changed");
+    private Label label;
+
+    Table t = new Table(3);
+
+    @FXML
+    public void onPressed(ActionEvent event){
+        t = new Table(3);
+        for (int i = 0; i<t.getTable().length; i++){
+            t.getTable()[i] = 0;
+            System.out.println("yeah");
+        }
+        Label label2 = new Label("O");
+        Label label3 = new Label("O");
+        Label label4 = new Label("O");
+        GridPane.setConstraints(label2, 0, 0);
+        GridPane.setConstraints(label3, 1, 0);
+        GridPane.setConstraints(label4, 2, 0);
+
+        grid.getChildren().addAll(label2, label3, label4);
     }
 
 }
---
+++ src/Table.java
@@ -0,0 +1,18 @@
+public class Table {
+
+    private int[] table;
+
+    public Table(int size){
+        this.table = new int[size];
+    }
+
+    public int[] getTable() {
+        return table;
+    }
+
+
+    public void setTable(int[] table) {
+        this.table = table;
+    }
+
+}
