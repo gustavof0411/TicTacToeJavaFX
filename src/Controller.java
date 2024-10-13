cee1bc2ba7fd7d32b2b9677ad81e79e13d47996c
Gustavo França <gustavofr0411@gmail.com>
2024-10-13 11:53:49 -0300

    Testing with circle and line shape components

--- src/Controller.java
+++ src/Controller.java
@@ -2,6 +2,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 public class Controller {
 
@@ -11,6 +14,9 @@ public class Controller {
     @FXML
     private GridPane grid;
 
+    @FXML
+    private Line linewin;
+
     Table t = new Table(3);
 
     @FXML
@@ -19,12 +25,20 @@ public class Controller {
         for (int i = 0; i<t.getTable().length; i++){
             for (int j = 0; j<t.getTable().length; j++){
                 t.getTable()[i][j] = 0;
-                Button button = new Button(Integer.toString(t.getTable()[i][j]));
-                GridPane.setConstraints(button,i,j);
-                System.out.println(t.getTable()[i][j]);
-                grid.getChildren().addAll(button);
+                Color stroke = new Color(0,0,0,1);
+                Color fill = new Color(0,0,0,0);          
+                Circle circle = new Circle(15, fill);
+                circle.setStroke(stroke);
+                //Button button = new Button(Integer.toString(t.getTable()[i][j]));
+                GridPane.setConstraints(circle,i,j);
+                grid.getChildren().addAll(circle);
             }
         }
+        linewin.setStartX(0);
+        linewin.setStartY(0);
+        linewin.setEndX(-100);
+        linewin.setEndY(-100);
+        linewin.setVisible(true);
     }
 
 }
